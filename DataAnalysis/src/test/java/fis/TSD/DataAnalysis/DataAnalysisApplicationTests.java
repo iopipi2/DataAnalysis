6807dc9f2c52b19c8be3ac21a102437810e0a88a
vietlq25 <@Iopipi12345>
2023-06-02 09:21:45 +0700

    Initial commit

---
+++ DataAnalysis/src/test/java/fis/TSD/DataAnalysis/DataAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.TSD.DataAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
